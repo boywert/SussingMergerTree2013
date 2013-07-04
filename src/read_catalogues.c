e92a5be90783d0bba221badda3dde4d266d3025c
Chaichalit Srisawat <cs390@tiberius.phys.susx.ac.uk>
2013-07-04 04:43:49 +0100

    autoupdate 2013-07-04-04:43:49

--- src/read_catalogues.c
+++ src/read_catalogues.c
@@ -438,7 +438,7 @@ int gadget_load_snapshot(char *fname, int files, struct Gadget_particle *P, int 
       printf("OmegaLambda : %lf\n",header1.OmegaLambda);
       printf("HubbleParam : %lf\n",header1.HubbleParam);
 
-      for(k=0;k<25;k++)
+      for(k=0;k<22;k++)
 	{
 	  printf("fillers[%d] : %d\n",k, header1.filler[k]);
 	}
