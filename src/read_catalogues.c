d2aae347433a28429ea3c5e004c42ca278db4427
Chaichalit Srisawat <cs390@tiberius.phys.susx.ac.uk>
2013-07-04 04:47:00 +0100

    autoupdate 2013-07-04-04:47:00

--- src/read_catalogues.c
+++ src/read_catalogues.c
@@ -442,7 +442,7 @@ int gadget_load_snapshot(char *fname, int files, struct Gadget_particle *P, int 
 	{
 	  printf("fillers[%d] : %d\n",k, header1.filler[k]);
 	}
-      fread(&dummy, sizeof(dummy), 1, fd);
+      fread(&dummy, sizeof(int), 1, fd);
       printf("dummy = %d\n",dummy);
       for(k = 0, pc_new = pc; k < 6; k++)
 	{
