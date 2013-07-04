a8f51be185811a43ac8560aade7808e67def0892
Chaichalit Srisawat <cs390@tiberius.phys.susx.ac.uk>
2013-07-04 12:26:36 +0100

    autoupdate 2013-07-04-12:26:36

--- src/read_catalogues.c
+++ src/read_catalogues.c
@@ -640,7 +640,7 @@ int gadget_load_snapshot(char *fname, int files, struct Gadget_particle *P, int 
   //printf("%d %f %f %f\n",Id[NumPart],P[NumPart].Pos[0],P[NumPart].Vel[0],P[NumPart].Mass);
   for(i=1;i<=header1.npartTotal[1];i++)
     {
-      if(Id[i] > header1.npartTotal[k]) printf("%d %f %f %f\n",(int)Id[i],P[i].Pos[0],P[i].Vel[0],P[i].Mass);
+      if(i > header1.npartTotal[k]) printf("%d %f %f %f\n",(int)Id[i],P[i].Pos[0],P[i].Vel[0],P[i].Mass);
       //printf("%d => %d\n",i,(int)Id[i]);
       //PIDmap[Id[i]] = i;
     }
