9af1bdd37a760b50d90da3c5feb8237bba24b1e8
Chaichalit Boyd Srisawat <boywert@boywert-X202E.(none)>
2014-07-04 13:24:33 +0100

    autoupdate

--- src/tools/create_subfind_catalogue_aq.c
+++ src/tools/create_subfind_catalogue_aq.c
@@ -16,7 +16,7 @@
 #define MINCOMMON      10   // we only cross-correlate haloes if they at least share MINCOMMON particles
 #define NSATinHOSTfrac 0.5  // at least NSATinHOSTfrac the subhaloes particles need to be inside the prospective host
 
-//#define SO_VEL_DISPERSIONS
+#define SO_VEL_DISPERSIONS
 
 //#define DEBUG
 //#define MTREE_SELF
@@ -26,7 +26,7 @@
 
 #define CLUES_WM3
 
-#define NSNAPS 1200
+#define NSNAPS 62
 #define MAXSTRING 4096
 #define MaxHiResID 18535972
 
@@ -68,8 +68,8 @@ struct halo_catalogue
   int *SubLen;
   int *SubParentHalo;
   int *IdToHalo;
-  float *Group_M_Mean200;
-  float *Group_R_Mean200;
+  float *Group_M_Crit200;
+  float *Group_R_Crit200;
   int *GroupNsubs;
   int *GroupLen;
   int *GroupFirstSub;
@@ -167,13 +167,13 @@ int main(int argc, char **argv)
   long long groupNsubs,groupNids,groupfirstsub,groupfirstid;
   double a_c,z;
   strcpy(SnapTimeFile,"data_snaplist.txt");
-  //strcpy(OUTfolder,"/mnt/lustre/scratch/cs390/datasetIII");
-  sprintf(OUTfolder,"/gpfs/data/Millgas/cs390/SUSSING2013/datasetIII/");
+  strcpy(OUTfolder,"/mnt/lustre/scratch/cs390/SUSSING2013_DATA/datasetII_spin/");
+  // sprintf(OUTfolder,"/gpfs/data/Millgas/cs390/SUSSING2013/datasetIII/");
   // use this with datalist_snap
-  //getSnapTime();
+  getSnapTime();
 
   sscanf(argv[1],"%d",&snapid);
-  sprintf(OutputDir,"/gpfs/data/aquarius/halo_data/Aq-A/4/");
+  sprintf(OutputDir,"/mnt/lustre/scratch/cs390/SUSSING2013_DATA/raw_subfind/");
   z = get_z_gadget(snapid);
   a_c = 1./(1+z);
   a[snapid] = (float)a_c;
@@ -308,16 +308,26 @@ int main(int argc, char **argv)
 	  ahf_id.v_esc = default_float;
 	  ahf_id.sigV = CatA.SubhaloVelDisp[k];
 	  ahf_id.lambda = default_float;
-	  ahf_id.lambdaE = default_float;
+
+	  normalise = sqrt(ahf_id.Lx*ahf_id.Lx + ahf_id.Ly*ahf_id.Ly + ahf_id.Lz*ahf_id.Lz);
+	  double G = 6.67384e-11; // m^3/(kgs^2
+	  double m2kpc = 1./3.08567758e19;
+	  double m2km = 0.001;
+	  double kg2Msun = 1./1.989e30;
+
+	  G *= m2kpc*pow(m2km,2.)/(kg2Msun);
+	  ahf_id.lambdaE = normalise / ahf_id.Mvir / sqrt(2. * G * ahf_id.Mvir * ahf_id.Rvir);
+	  absAngMom / halo->M_vir / sqrt(2. * Grav * halo->M_vir * halo->R_vir);
+	  /* ahf_id.Lx = default_float; */
+	  /* ahf_id.Ly = default_float; */
+	  /* ahf_id.Lz = default_float; */
 	  
-	  ahf_id.Lx = default_float;
-	  ahf_id.Ly = default_float;
-	  ahf_id.Lz = default_float;
 	  
-	  //normalise = sqrt(ahf_id.Lx*ahf_id.Lx + ahf_id.Ly*ahf_id.Ly + ahf_id.Lz*ahf_id.Lz);
-	  //ahf_id.Lx /= normalise;
-	  //ahf_id.Ly /= normalise;
-	  //ahf_id.Lz /= normalise;
+	  ahf_id.Lx /= normalise;
+	  ahf_id.Ly /= normalise;
+	  ahf_id.Lz /= normalise;
+
+	  
 	  
 	  ahf_id.b = default_float;
 	  ahf_id.c = default_float;
@@ -568,12 +578,12 @@ void load_subhalo_catalogue(int num, struct halo_catalogue *cat)
       fseek(fd, sizeof(int) * ngroups, SEEK_CUR);	/* skip  GroupOffset  */
       fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  GroupMass  */
       fseek(fd, 3 * sizeof(float) * ngroups, SEEK_CUR);	/* skip  GroupPos */
-      //fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_M_Mean200 */
-      my_fread(&cat->Group_M_Mean200[groupcount], sizeof(float), ngroups, fd);
-      //fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_R_Mean200 */
-      my_fread(&cat->Group_R_Mean200[groupcount], sizeof(float), ngroups, fd);
-      fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_M_Crit200 */
-      fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_R_Crit200 */
+      fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_M_Mean200 */
+      fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_R_Mean200 */
+      my_fread(&cat->Group_M_Crit200[groupcount], sizeof(float), ngroups, fd);
+      my_fread(&cat->Group_R_Crit200[groupcount], sizeof(float), ngroups, fd);
+      // fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_M_Crit200 */
+      // fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_R_Crit200 */
       fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_M_TopHat200 */
       fseek(fd, sizeof(float) * ngroups, SEEK_CUR);	/* skip  Group_R_TopHat200 */
 #ifdef SO_VEL_DISPERSIONS
